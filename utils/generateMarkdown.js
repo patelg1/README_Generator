1793d6e263408b4db636bca2dc825dd26f8fe94e
Gopal Patel <patelgopalk1@gmail.com>
2021-02-26 19:22:35 -0500

    Added license badge and license section functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,83 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  // var returnString = '';  
+  // if (license === 'Apache License 2.0'){
+  //   returnString = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+  // }else if (license === 'GNU AGPLv3'){
+  //   returnString = '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)'
+  // }else if (license === 'GNU GPLv3'){
+  //   returnString = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+  // }else if (license === 'GNU LGPLv3'){
+  //   returnString = '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)'
+  // }else if (license === 'Mozilla Public License 2.0'){
+  //   returnString = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
+  // }else if (license === 'MIT License'){
+  //   returnString = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  // }else if (license === 'Boost Software License 1.0'){
+  //   returnString = '[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)'
+  // }else if (license === 'The Unlicense'){
+  //   returnString = '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)'
+  // }  
+  
+  // return returnString;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  var returnString = '';
+  if (license === 'Apache License 2.0'){
+    returnString = 'A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.'
+  }else if (license === 'GNU AGPLv3'){
+    returnString = 'Permissions of this strongest copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. When a modified version is used to provide a service over a network, the complete source code of the modified version must be made available.'
+  }else if (license === 'GNU GPLv3'){
+    returnString = 'Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.'
+  }else if (license === 'GNU LGPLv3'){
+    returnString = 'Permissions of this copyleft license are conditioned on making available complete source code of licensed works and modifications under the same license or the GNU GPLv3. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work through interfaces provided by the licensed work may be distributed under different terms and without source code for the larger work.'
+  }else if (license === 'Mozilla Public License 2.0'){
+    returnString = 'Permissions of this weak copyleft license are conditioned on making available source code of licensed files and modifications of those files under the same license (or in certain cases, one of the GNU licenses). Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work may be distributed under different terms and without source code for files added in the larger work.'
+  }else if (license === 'MIT License'){
+    returnString = 'A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.'
+  }else if (license === 'Boost Software License 1.0'){
+    returnString = 'A simple permissive license only requiring preservation of copyright and license notices for source (and not binary) distribution. Licensed works, modifications, and larger works may be distributed under different terms and without source code.'
+  }else if (license === 'The Unlicense'){
+    returnString = 'A license with no conditions whatsoever which dedicates works to the public domain. Unlicensed works, modifications, and larger works may be distributed under different terms and without source code.'
+  }  
+
+  return returnString;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  var returnString = '';
+  if (license === 'Apache License 2.0'){
+    returnString = 'A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.'
+  }else if (license === 'GNU AGPLv3'){
+    returnString = 'Permissions of this strongest copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. When a modified version is used to provide a service over a network, the complete source code of the modified version must be made available.'
+  }else if (license === 'GNU GPLv3'){
+    returnString = 'Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.'
+  }else if (license === 'GNU LGPLv3'){
+    returnString = 'Permissions of this copyleft license are conditioned on making available complete source code of licensed works and modifications under the same license or the GNU GPLv3. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work through interfaces provided by the licensed work may be distributed under different terms and without source code for the larger work.'
+  }else if (license === 'Mozilla Public License 2.0'){
+    returnString = 'Permissions of this weak copyleft license are conditioned on making available source code of licensed files and modifications of those files under the same license (or in certain cases, one of the GNU licenses). Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work may be distributed under different terms and without source code for files added in the larger work.'
+  }else if (license === 'MIT License'){
+    returnString = 'A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.'
+  }else if (license === 'Boost Software License 1.0'){
+    returnString = 'A simple permissive license only requiring preservation of copyright and license notices for source (and not binary) distribution. Licensed works, modifications, and larger works may be distributed under different terms and without source code.'
+  }else if (license === 'The Unlicense'){
+    returnString = 'A license with no conditions whatsoever which dedicates works to the public domain. Unlicensed works, modifications, and larger works may be distributed under different terms and without source code.'
+  }  
+
+  return returnString;
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
 
   ## Description
   ${data.description}
@@ -38,10 +103,11 @@ function generateMarkdown(data) {
   ${data.tests}
 
   ## License
-  ${data.license}
+  ${data.license} <br />
+  ${renderLicenseSection(data.license)}
 
   ## Questions
-  Github: https://github.com/${data.username}
+  Github: https://github.com/${data.username} <br />
   Email: ${data.email}
 
 `;
